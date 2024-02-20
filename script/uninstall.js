ccf596effe1e2122198df3795464ded31c1e9a88
MonshinYu <MonshinYu@gmail.com>
2024-02-20 17:29:51 +0800

    init

--- script/uninstall.js
+++ script/uninstall.js
@@ -1,3 +1,3 @@
 const path = require('path');
 const { execSync } = require('child_process');
-execSync(path.join(__dirname,'..','sh','uninstall.sh'));
+execSync(`bash ${path.join(__dirname,'..','sh','uninstall.sh')}`);
